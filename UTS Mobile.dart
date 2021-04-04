d9cb081ceb632f82fbbcb095c8afed9830cb08ed
Indra-Pramana <80260627+Indra-Pramana@users.noreply.github.com>
2021-04-04 21:40:17 +0700

    Add files via upload

--- UTS Mobile.dart
+++ UTS Mobile.dart
@@ -13,12 +13,26 @@ void main()
     Widget build(BuildContext context){
       return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(title: Text("aplikasi Saya"),),
-          body: Center(child: Container,),
-          color: Colors.navy,
+          appBar: AppBar(title: Text("Latihan Container"),
+          // AppBar
+      
+          body: Container(
+          margin: Edgeinsets.fromLTRB(5,10,15,20),
+          Paddling: Eddgeinsets.only(bottom: 10,Right:10, left:10, top:10),
+          color: Colors.blue,
           width: 150,
           height: 50,
-          child: Text("Aplikasi pertama saya")
+          child: Container(
+            margin: EdgeInsert.all(10)
           style: TextStyle(color: Colors.white, fontStyle: FontStyle.hold),
+       }, // Container
+     }, // Container
+    }, // Scaffold
+  }, // MaterialApp
+
+  
+  
+  
+
 
           
